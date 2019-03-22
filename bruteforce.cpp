0e5f3257055714a994498ac2459762e65d48289e
shubham <4dman.com@gmail.com>
2019-03-22 14:04:52 +0530

    Update bruteforce.cpp
    Made some optimizations. First of all, for the future, remember to assign and pass by reference whenever possible, instead of copying whole objects. Also, there was an omission on (now) line 42.

--- bruteforce.cpp
+++ bruteforce.cpp
@@ -1,17 +1,18 @@
 //This is totally independent of the rest of the project
+
 #include<bits/stdc++.h>
 using namespace std;
 #define watchv(v) cout << (#v) << " is: ["; for(auto itr = v.begin(); itr!=v.end();itr++){cout<<*itr<<", ";}cout<<(v.empty()?"":"\b\b")<<"] \n"
 //the following two are for vector of vectors and like.
 #define watchvunnamed(v) cout << "["; for(auto itt = v.begin(); itt!=v.end();itt++){cout<<*itt<<", ";} cout<<(v.empty()?"":"\b\b")<<"]"
 #define watchvv(v) cout << (#v) << " is: ["; for(auto itr = v.begin(); itr!=v.end();itr++){watchvunnamed((*itr)); cout<<", ";} cout<<(v.empty()?"":"\b\b")<<"] \n"
-int f(int i){
-    if(i<=2)return 1;
-    if(i<=5)return 4;
-    return 7;
-}
 
-bool allowed(int I, int J, char r, vector<vector<char>> ret){
+/***************************************************************************************************************************/
+/***************************************************************************************************************************/
+
+#define f(x) x<=2?1:(x<=5?4:7)
+
+bool allowed(int I, int J, char r, vector<vector<char>> &ret){
     for(int i = 0; i<9; i++){
         if(i!=I && ret[i][J]==r) return false;
     }
@@ -25,19 +26,11 @@ bool allowed(int I, int J, char r, vector<vector<char>> ret){
             if(ret[IR+i][JR+j]==r) return false;
         }
     }
-    
     return true;
 }
-bool filled(vector<vector<char>> &ret){
-    for(auto i:ret){
-        for(auto j: i){
-            if(j=='.')return false;
-        }
-    }
-    return true;
-}
+
 bool solve(vector<vector<char>> &ret){
-    for(int I=0; I<9; I++){ vector<char> i = ret[I];
+    for(int I=0; I<9; I++){ vector<char> &i = ret[I];
         for(int J=0; J<9; J++){ char j = i[J];
             if(j=='.'){
                 bool none = true;
@@ -46,21 +39,19 @@ bool solve(vector<vector<char>> &ret){
                         none = false;
                         ret[I][J] = r;
                         if(solve(ret))return true;
-                        else{ret[I][J]='.';}
+                        else{ret[I][J]='.'; none = true;}
                     }
                 }
                 if(none) return false;
-            }cc
+            }
             if(I==8 && J==8)return true;
-            //if(filled(ret)) return true;
         }
     }
 }
 void solveSudoku(vector<vector<char> > &A) {
-    vector<vector<char> > ret(A.begin(), A.end());
     solve(A);
 }
-//2147483647
+
 int main(){
     vector<vector<char> > v={{'5', '3', '4', '6', '7', '8', '9', '1', '2'}, {'6', '7', '2', '1', '9', '5', '3', '4', '8'}, {'1', '9', '8', '3', '4', '2', '5', '6', '7'}, {'8', '5', '9', '7', '6', '1', '4', '2', '3'}, {'4', '2', '6', '8', '5', '3', '7', '9', '1'}, {'7', '1', '3', '9', '2', '4', '8', '5', '6'}, {'9', '6', '1', '5', '3', '7', '2', '8', '4'}, {'2', '8', '7', '4', '1', '9', '6', '3', '5'}, {'3', '4', '5', '2', '8', '6', '1', '7', '9'}};
     solveSudoku(v);
